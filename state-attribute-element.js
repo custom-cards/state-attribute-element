3cdc42de5c4f379fa162aa3cf1150a6691d5c28c
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-07-25 15:16:34 +0200

    Add option to set prefix for the attribute (#1)

--- state-attribute-element.js
+++ state-attribute-element.js
@@ -1,10 +1,15 @@
 class StateAttributeElement extends HTMLElement {
   set hass(hass) {
     const entityId = this.config.entity;
+    const prefix_string = this.config.prefix
     const attr = this.config.attribute;
     const state = hass.states[entityId].attributes[attr];
     const card = document.createElement('state-attribute-element');
-    this.innerHTML = state;
+    if (prefix_string) {
+      this.innerHTML = prefix_string + state;
+    } else {
+      this.innerHTML = state;
+    }
   }
   setConfig(config) {
     if (!config.entity) {
