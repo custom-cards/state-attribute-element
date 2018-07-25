23d1155e2d7e4eba282e03392ddfe5709f94ac26
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-07-25 17:29:40 +0200

    Added show_empty option (#2)

--- state-attribute-element.js
+++ state-attribute-element.js
@@ -2,13 +2,22 @@ class StateAttributeElement extends HTMLElement {
   set hass(hass) {
     const entityId = this.config.entity;
     const prefix_string = this.config.prefix
+    const show_empty = this.config.show_empty
     const attr = this.config.attribute;
     const state = hass.states[entityId].attributes[attr];
     const card = document.createElement('state-attribute-element');
-    if (prefix_string) {
-      this.innerHTML = prefix_string + state;
-    } else {
-      this.innerHTML = state;
+    if (state != null) {
+      if (prefix_string) {
+        this.innerHTML = prefix_string + state;
+      } else {
+        this.innerHTML = state;
+      }
+    } else if (show_empty == true) {
+      if (prefix_string) {
+        this.innerHTML = prefix_string + state;
+      } else {
+        this.innerHTML = state;
+      }
     }
   }
   setConfig(config) {
@@ -17,6 +26,9 @@ class StateAttributeElement extends HTMLElement {
     }
     if (!config.attribute) {
       throw new Error('You need to define an attribute');
+    }
+    if (!config.show_empty) {
+      config.show_empty = false;
     }
     this.config = config;
   }
