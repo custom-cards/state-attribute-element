fc8533f2d0632763eb6f8f8a187868972d31ebca
Joakim Sørensen <joasoe@gmail.com>
2018-10-20 18:41:38 +0200

    Add support for sub_attribute

--- state-attribute-element.js
+++ state-attribute-element.js
@@ -4,11 +4,15 @@ class StateAttributeElement extends HTMLElement {
     const prefix_string = this.config.prefix || ''
     const suffix_string = this.config.suffix || ''
     const show_empty = this.config.show_empty
-    const attr = this.config.attribute;
-    const state = hass.states[entityId].attributes[attr];
+    const attr = this.config.attribute
+    const sub_attribute = this.config.sub_attribute || ''
+    this.state = hass.states[entityId].attributes[attr]
+    if (this.config.sub_attribute) {
+      this.state = hass.states[entityId].attributes[attr][sub_attribute]
+    }
     const card = document.createElement('state-attribute-element');
-    if (state.length != 0 || show_empty === true) {
-      this.innerHTML = `${prefix_string}${state}${suffix_string}`
+    if (this.state.length != 0 || show_empty === true) {
+      this.innerHTML = `${prefix_string}${this.state}${suffix_string}`
     }
   }
   setConfig(config) {
